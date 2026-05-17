13b19e1552d1e7450e12c0eac27329150e22d247
ionlyAamir <amir_233012@saitm.ac.in>
2026-05-17 22:06:57 +0530

    add new code and files

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+const port = 2000;
+
+app.get("/rrot", (req, res) => {
+    res.send("this is root path");
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
+
