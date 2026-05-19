496a81c733447d3e41d6d2ae38fdc3e86474d32a
ionlyAamir <amir_233012@saitm.ac.in>
2026-05-19 18:13:44 +0530

    add code

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.get("/about", (req, res) => {
      res.send("this is about path");
 })
 
+app.get("/:username", (req, res) => {
+    let {username} = req.params;
+    res.send(`this is ${username} path`);
+})
+
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 })
