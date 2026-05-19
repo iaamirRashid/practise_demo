754ed7e0c1742664c0ecf8c79ec51c53db644699
ionlyAamir <amir_233012@saitm.ac.in>
2026-05-19 18:16:18 +0530

    add code

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ app.get("/:username", (req, res) => {
     res.send(`this is ${username} path`);
 })
 
+app.get("/search", (req, res) => {
+    let {q} = req.query;
+    if(!q) {
+        res.send("No search query provided");
+    } else {
+        res.send(`there are the results for : ${q}`);
+    }
+})
+
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 })
