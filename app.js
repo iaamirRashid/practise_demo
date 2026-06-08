0d4c251da89c1f3eeff41fbaba09c136aba1d866
iaamirRashid <aamironlyaamir@gmail.com>
2026-06-09 01:57:39 +0530

    Remove incorrect root path endpoint

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const app = express();
 
 const port = 2000;
 
-app.get("/rrot", (req, res) => {
-    res.send("this is root path");
-})
+
 app.get("/:username", (req, res) => {
     let {username} = req.params;
     res.send(`this is ${username} path`);
